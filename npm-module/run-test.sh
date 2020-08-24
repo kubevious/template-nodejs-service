7eeb846fc5c65d3f76d94a99fcf86ab36c586dd3
Ruben Hakopian <ruben.hakopian@gmail.com>
2020-08-24 16:07:01 -0700

    added tests

--- npm-module/run-test.sh
+++ npm-module/run-test.sh
@@ -3,9 +3,4 @@ MY_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE
 MY_DIR="$(dirname $MY_PATH)"
 cd $MY_DIR
 
-rm -rf node_modules/
-rm -rf dist/
-
-npm install
-npm install --only=dev
-npm update the-lodash the-logger the-promise
+mocha
---
+++ npm-module/test/module-import.ts
@@ -0,0 +1,12 @@
+import 'mocha';
+import 'should';
+// import aadddd from '../src';
+
+describe('First test', 
+  () => { 
+    it('should return true', () => { 
+    //   const result = helloTest();
+    //   expect(result).to.equal(true); 
+  }); 
+});
+
