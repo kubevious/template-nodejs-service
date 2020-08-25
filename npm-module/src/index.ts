7d3fa15562d82ab2c18c14700fea5ba99e983c4b
Ruben Hakopian <ruben.hakopian@gmail.com>
2020-08-24 17:06:26 -0700

    added class examples

--- npm-module/src/index.ts
+++ npm-module/src/index.ts
@@ -1,5 +1,11 @@
+import Calculator from './calculator';
+
+let cal = new Calculator();
+
 export function add(a: number, b: number) {
-  return a + b;
+    return cal.add(a, b);
 }
+
+export { Calculator };
 
 console.log('Hello From Sample TS Lib!!!');
